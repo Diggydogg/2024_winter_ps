6aca4696b70dd804f5f5cf51adbc584bf0ffc6da
DiggyDogg <70377019+Diggydogg@users.noreply.github.com>
2025-01-05 02:14:30 +0900

    Update p42557.java

---
+++ p42557.java
@@ -0,0 +1,26 @@
+package week1;
+//https://school.programmers.co.kr/learn/courses/30/lessons/42577
+// 전화번호 목록 프로그래머스
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        phone_book = Arrays.stream(phone_book).sorted((s1, s2)->Integer.compare(s1.length(), s2.length())).toArray(String[]::new); // actually doesnt matter at all
+        HashSet <String> h1 = new HashSet<>();
+        for(String phone_number:phone_book){
+            if(h1.isEmpty()) h1.add(phone_number);
+            else {
+                for(int i = 0; i < phone_number.length(); i++){
+                    String pre = phone_number.substring(0,i);
+                    if(h1.contains(pre)) return false;
+                }
+                h1.add(phone_number);
+            }
+        }
+        
+        boolean answer = true;
+        return answer;
+    }
+}
